d94d48bc4e8518eefca573d606ea695925f36a5c
DEEPAK NAIK <DEEPAK NAIK@LAPTOP-6EAKNGIG>
2022-12-15 19:46:59 +0530

    Added new

---
+++ Jobbers Cafe/src/Object_class/Wrapper_class.java
@@ -0,0 +1,15 @@
+package Object_class;
+
+public class Wrapper_class {
+public static void main(String[] args) {
+	int obj=10;
+	double obj1 =10.25389;
+	long obj2= 1234567896321L;
+	float obj3=10.3f;
+	char obj4= 'c';
+	boolean obj5 = true;
+	
+	// String str="jobbers cafe"
+
+}
+}
